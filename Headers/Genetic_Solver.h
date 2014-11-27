00ca337f71c1d8a7a4acb2679b31ce4863803bcb
Valentin Montmirail <valentin.montmirail@gmail.com>
2014-11-27 11:58:43 +0100

    Ajout de la 'classe' Genetic_Solver avec les déclarations des fonctions

---
+++ Headers/Genetic_Solver.h
@@ -0,0 +1,36 @@
+#ifndef GENETIC_SOLVER_H
+#define GENETIC_SOLVER_H
+
+#include "Solver.h"
+
+/** 
+ * We define our structure for our Genetic_Solver.
+ * It's composed from a number of individual
+ * and the population of solver.
+ */
+struct _Genetic_Solver {
+
+	unsigned int nbIndividual;
+
+	Solver* population;
+
+};
+
+/** We rename our structure to avoid to tape struct _Genetic_Solver everywhere... */
+typedef struct _Genetic_Solver Genetic_Solver;
+
+Genetic_Solver* initGeneticSolver(Problem* problem);
+
+unsigned int isSolutionG(Genetic_Solver* solver, Problem* problem);
+
+unsigned int isOpenG(Genetic_Solver* solver, Clause* clause);
+
+void displaySolutionG(Genetic_Solver* solver);
+
+void solveProblemG(Genetic_Solver* solver, Problem* problem);
+
+void crossing(Genetic_Solver* solver);
+
+void mutation(Genetic_Solver* solver);
+
+#endif
