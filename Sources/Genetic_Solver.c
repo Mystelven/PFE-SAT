beed992b9789b91a5c245fd431a5732ba3eab05f
Valentin Montmirail <valentin.montmirail@gmail.com>
2014-11-27 11:58:43 +0100

    Ajout de la 'classe' Genetic_Solver avec les déclarations des fonctions

---
+++ Sources/Genetic_Solver.c
@@ -0,0 +1,43 @@
+#include "../Headers/Genetic_Solver.h"
+
+Genetic_Solver* initGeneticSolver(Problem* problem) {
+
+
+	return NULL;
+}
+
+
+unsigned int isSolutionG(Genetic_Solver* solver, Problem* problem) {
+
+
+	return 0;
+}
+
+unsigned int isOpenG(Genetic_Solver* solver, Clause* clause) {
+
+
+	return 0;
+}
+
+
+void displaySolutionG(Genetic_Solver* solver) {
+
+
+}
+
+void solveProblemG(Genetic_Solver* solver, Problem* problem) {
+
+
+}
+
+void crossing(Genetic_Solver* solver) {
+
+
+
+}
+
+void mutation(Genetic_Solver* solver) {
+
+
+
+}
