9eee02eef10d276ca8c1ef6a6927710acbb0186d
Mystelven <valentin.montmirail@gmail.com>
2015-02-10 16:28:15 +0100

    multiplication des benchmarks afin d'obtenir 1000 dans chaque catégorie

--- test.sh
+++ test.sh
@@ -35,8 +35,7 @@ function test()
 	#testUf $1 "uf20/" 20
 	#testUf $1 "uf50/" 50
 	#testUf $1 "uf75/" 75 
-	testUf $1 "uf100/" 100 
-	#testUf $1 "sw/" 100 
+	#testUf $1 "uf100/" 100 
 	#testUf $1 "uf125/" 125 
 	#testUf $1 "uf150/" 150
 	#testUf $1 "uf175/" 175
@@ -44,15 +43,9 @@ function test()
 	#testUf $1 "uf225/" 225
 	#testUf $1 "uf250/" 250
 
-	#testUf $1 "uuf50/" 50
-	#testUf $1 "uuf75/" 75
+	testUf $1 "uuf50/" 50
 	#testUf $1 "uuf100/" 100
-	#testUf $1 "uuf125/" 125
- 	#testUf $1 "uuf150/" 150
-	#testUf $1 "uuf175/" 175
 	#testUf $1 "uuf200/" 200
-	#testUf $1 "uuf225/" 225
-	#testUf $1 "uuf250/" 250
 
 	echo ""
 	result $1 
