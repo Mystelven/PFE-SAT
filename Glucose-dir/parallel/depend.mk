f2ae48d058a61e4dbf4f652a8e1047555a5b644c
Mystelven <valentin.montmirail@gmail.com>
2015-02-17 11:58:41 +0100

    normalement le git est revenu en place là

---
+++ Glucose-dir/parallel/depend.mk
@@ -0,0 +1,181 @@
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ClausesBuffer.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ClausesBuffer.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ClausesBuffer.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ClausesBuffer.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ClausesBuffer.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/Main.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/Main.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/Main.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/Main.op: /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/Main.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/System.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Dimacs.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ParallelSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SharedCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/MultiSolvers.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/MultiSolvers.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/MultiSolvers.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/MultiSolvers.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/MultiSolvers.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/MultiSolvers.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/MultiSolvers.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ParallelSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SharedCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Sort.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/System.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverConfiguration.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ParallelSolver.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ParallelSolver.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ParallelSolver.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ParallelSolver.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/ParallelSolver.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ParallelSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SharedCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Sort.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SharedCompanion.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SharedCompanion.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SharedCompanion.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SharedCompanion.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SharedCompanion.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ParallelSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SharedCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverCompanion.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverCompanion.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverCompanion.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverCompanion.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverCompanion.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverConfiguration.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverConfiguration.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverConfiguration.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverConfiguration.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/SolverConfiguration.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/MultiSolvers.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ParallelSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SharedCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverCompanion.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/ClausesBuffer.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/parallel/../parallel/SolverConfiguration.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/Main.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/Main.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/Main.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/Main.op: /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/Main.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/System.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/Dimacs.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Clone.h
+/Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/SimpSolver.o /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/SimpSolver.or /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/SimpSolver.od /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/SimpSolver.op: \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/SimpSolver.cc \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Sort.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Vec.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/IntTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/XAlloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../simp/SimpSolver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Queue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/Solver.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Heap.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Alg.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/Options.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/ParseUtils.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/SolverTypes.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Map.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Alloc.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/BoundedQueue.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../core/Constants.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../mtl/Clone.h \
+  /Users/mystelven/Desktop/PFE-SAT/Glucose-dir/simp/../utils/System.h
