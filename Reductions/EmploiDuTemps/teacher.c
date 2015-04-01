10fede16a94d3825e3c30c6c56a5dd001e4bf210
Mystelven <valentin.montmirail@gmail.com>
2015-04-01 15:23:55 +0200

    ajout de documentation sur le projet pour augmenter le sonar

--- Reductions/EmploiDuTemps/teacher.c
+++ Reductions/EmploiDuTemps/teacher.c
@@ -17,9 +17,16 @@ Teacher* createTeacher() {
 	return result;
 }
 
+/**
+ * addIntervalToTeacher : this function allow to add the reference of an interval to a teacher.
+ * like this, it means that the teacher is able to teach during this interval. 
+ * @param teacher The teacher that is able to teach something at one time.
+ * @param interval the interval authorized for the teacher.
+ */
 void addIntervalToTeacher(Teacher* teacher, Interval* interval) {
 
 	if(teacher->nbInterval == teacher->sizeArray) {
+		
 		teacher->array_intervalPossible = realloc(teacher->array_intervalPossible, teacher->sizeArray*2*sizeof(Interval*));
 		teacher->sizeArray *= 2;
 	}
