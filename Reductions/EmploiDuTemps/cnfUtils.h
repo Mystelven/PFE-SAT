e19448962c6f04876585b854e6192855b8748097
Mystelven <valentin.montmirail@gmail.com>
2015-04-02 09:52:23 +0200

    test correction indentation Sonar

--- Reductions/EmploiDuTemps/cnfUtils.h
+++ Reductions/EmploiDuTemps/cnfUtils.h
@@ -15,7 +15,6 @@
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
  * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
  */
 
 #ifndef CNF_UTILS_H
@@ -30,14 +29,14 @@
  * @param filename the name of the input file.
  * @return a Planning structure. 
  */
-Planning * readInputFile(const char* filename);
+ Planning * readInputFile(const char* filename);
 
 /**
  * createCNF : This function will create a CNF file according to a Planning.
  * @param planning the planning that we want to solve. 
  * @return the filename of the CNF file created.
  */
-char* createCNF(Planning* planning);
+ char* createCNF(Planning* planning);
 
 /**
  * getSolutionSchedule : This function allow to get in an integer array, the id of all the intervals solutions.
@@ -45,25 +44,25 @@ char* createCNF(Planning* planning);
  * @param planning the planning who contains the all problem.
  * @return an array of integer who contains the ID of intervals solutions.
  */
-unsigned int* getSolutionSchedule(Planning* planning,const char* s);
+ unsigned int* getSolutionSchedule(Planning* planning,const char* s);
 
 /**
  * getNbVariables : This function allows us to get the number of boolean variables.
  * @return the number of boolean variables in the CNF file.
  */
-inline unsigned int getNbVariables(Planning* planning);
+ inline unsigned int getNbVariables(Planning* planning);
 
 /**
  * getNbConstraint : This function allows us to get the number of clauses.
  * @return the number of clauses in the CNF file.
  */
-unsigned int getNbConstraint(Planning* planning);
+ unsigned int getNbConstraint(Planning* planning);
 
 
-void displaySolutionSchedule(FILE* output, Planning* planning,unsigned int* solution);
+ void displaySolutionSchedule(FILE* output, Planning* planning,unsigned int* solution);
 
-int isSolutionExisting(Planning* planning, unsigned int* solution);
+ int isSolutionExisting(Planning* planning, unsigned int* solution);
 
-void writeOneIntervalDontOverlap(FILE* file, Planning* planning);
+ void writeOneIntervalDontOverlap(FILE* file, Planning* planning);
 
 #endif 
--- Reductions/EmploiDuTemps/htmlSolutionGenerator.c
+++ Reductions/EmploiDuTemps/htmlSolutionGenerator.c
@@ -23,17 +23,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-const char* headerHTML = "<style type=\"text/css\">body { margin:5px; }.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc; margin:auto;} .tg td{ width: 140px; font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;} .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;} .tg .tg-59zk{font-weight:bold;font-family:\"Lucida Console\", Monaco, monospace !important;;background-color:#efefef;text-align:center} .tg .tg-xahr{font-family:\"Lucida Console\", Monaco, monospace !important;;background-color:#efefef;text-align:center} .tg .tg-slkj{font-family:\"Lucida Console\", Monaco, monospace !important;;text-align:center} .tg .tg-kkwu{font-weight:bold;font-family:\"Lucida Console\", Monaco, monospace !important;;text-align:center} </style> <table class=\"tg\"><tr> <th class=\"tg-kkwu\">-----------</th> <th class=\"tg-kkwu\">Monday</th> <th class=\"tg-kkwu\">Tuesday</th> <th class=\"tg-kkwu\">Wednesday</th> <th class=\"tg-kkwu\">Thursday</th> <th class=\"tg-kkwu\">Friday</th> </tr>";
+ const char* headerHTML = "<style type=\"text/css\">body { margin:5px; }.tg  {border-collapse:collapse;border-spacing:0;border-color:#ccc; margin:auto;} .tg td{ width: 140px; font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#fff;} .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#ccc;color:#333;background-color:#f0f0f0;} .tg .tg-59zk{font-weight:bold;font-family:\"Lucida Console\", Monaco, monospace !important;;background-color:#efefef;text-align:center} .tg .tg-xahr{font-family:\"Lucida Console\", Monaco, monospace !important;;background-color:#efefef;text-align:center} .tg .tg-slkj{font-family:\"Lucida Console\", Monaco, monospace !important;;text-align:center} .tg .tg-kkwu{font-weight:bold;font-family:\"Lucida Console\", Monaco, monospace !important;;text-align:center} </style> <table class=\"tg\"><tr> <th class=\"tg-kkwu\">-----------</th> <th class=\"tg-kkwu\">Monday</th> <th class=\"tg-kkwu\">Tuesday</th> <th class=\"tg-kkwu\">Wednesday</th> <th class=\"tg-kkwu\">Thursday</th> <th class=\"tg-kkwu\">Friday</th> </tr>";
 
-const char* footerHTML = "</table>";
+ const char* footerHTML = "</table>";
 
-	int	correspondances[5][6] = {
-								{8,10,12,14,16,18} , 
-								{32,34,36,38,40,42}, 
-								{56,58,60,62,64,66}, 
-								{80,82,84,86,88,90}, 
-								{104,106,108,110,112,114} 
-								};
+ int	correspondances[5][6] = {
+    {8,10,12,14,16,18} , 
+    {32,34,36,38,40,42}, 
+    {56,58,60,62,64,66}, 
+    {80,82,84,86,88,90}, 
+    {104,106,108,110,112,114} 
+};
 
 char* getDisplaySubjectAndRoom(Planning* planning, unsigned int* solution, int ligne, int colonne) {
 
@@ -68,7 +68,7 @@ char* getDisplaySubjectAndRoom(Planning* planning, unsigned int* solution, int l
 					
 
 					start  = (int)start;
-			
+                 
 					if( ((int)(start - correspondances[colonne][ligne])) != 0) {
 						
 						continue;
@@ -125,97 +125,97 @@ char* createHTMLplan(Planning* planning, unsigned int* solution,double generatio
 		}
 
 		fprintf(file,"%s","<tr>");
-    	fprintf(file,"%s","<td class=\"tg-59zk\">");
+       fprintf(file,"%s","<td class=\"tg-59zk\">");
 
-    	switch(i) {
+       switch(i) {
 
-    		case 0:
-    			fprintf(file,"%s","08:15 - 10:15");
-    			break;
-    		case 1:
-    			fprintf(file,"%s","10:15 - 12:30");
-    			break;
+          case 0:
+          fprintf(file,"%s","08:15 - 10:15");
+          break;
+          case 1:
+          fprintf(file,"%s","10:15 - 12:30");
+          break;
 
-    		case 3:
-    			fprintf(file,"%s","14:00 - 16:00");
-    			break;
-    		case 4:
-    			fprintf(file,"%s","16:15 - 18:15");
-    			break;
-    	}
-    	fprintf(file,"%s","</td>");
+          case 3:
+          fprintf(file,"%s","14:00 - 16:00");
+          break;
+          case 4:
+          fprintf(file,"%s","16:15 - 18:15");
+          break;
+      }
+      fprintf(file,"%s","</td>");
 
     	// ----------------------------------------
 
-    	fprintf(file,"%s","<td class=\"tg-slkj\">");
+      fprintf(file,"%s","<td class=\"tg-slkj\">");
 
     		// 1re colonne
-    		fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,0));
+      fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,0));
 
-    	fprintf(file,"%s","</td>");
+      fprintf(file,"%s","</td>");
 
     	// ----------------------------------------
 
-    	fprintf(file,"%s","<td class=\"tg-slkj\">");
-    		
+      fprintf(file,"%s","<td class=\"tg-slkj\">");
+      
     		// 2e colonne
-    		fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,1));
+      fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,1));
 
-    	fprintf(file,"%s","</td>");
+      fprintf(file,"%s","</td>");
 
     	// ----------------------------------------
 
-    	fprintf(file,"%s","<td class=\"tg-slkj\">");
+      fprintf(file,"%s","<td class=\"tg-slkj\">");
 
     		// 3e colonne
-    		fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,2));
+      fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,2));
 
-    	fprintf(file,"%s","</td>");
+      fprintf(file,"%s","</td>");
 
     	// ----------------------------------------
 
-    	fprintf(file,"%s","<td class=\"tg-slkj\">");
+      fprintf(file,"%s","<td class=\"tg-slkj\">");
     		// 4e colonne
-    		fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,3));
+      fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,3));
 
-    	fprintf(file,"%s","</td>");
+      fprintf(file,"%s","</td>");
 
     	// ----------------------------------------
 
-    	fprintf(file,"%s","<td class=\"tg-slkj\">");
+      fprintf(file,"%s","<td class=\"tg-slkj\">");
 
     		// 5e colonne
-    		fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,4));
+      fprintf(file,"%s",getDisplaySubjectAndRoom(planning,solution,i,4));
 
-    	fprintf(file,"%s","</td>");
+      fprintf(file,"%s","</td>");
 
 		// ----------------------------------------
 
-    	fprintf(file,"%s","</tr>");
-  	}
+      fprintf(file,"%s","</tr>");
+  }
 
-  	fprintf(file,"%s","<div style=\"float:left;\" ><ul>");
+  fprintf(file,"%s","<div style=\"float:left;\" ><ul>");
 
-  	char buf[1024];
-  	FILE* fileInput = fopen(input,"r+");
-  	while(fgets(buf,sizeof(buf),fileInput)) {
-  		if(buf[0] == 'c')
-  			fprintf(file,"<li>%s</li>",buf+1);
-  	}
-  	fclose(fileInput);
+  char buf[1024];
+  FILE* fileInput = fopen(input,"r+");
+  while(fgets(buf,sizeof(buf),fileInput)) {
+    if(buf[0] == 'c')
+       fprintf(file,"<li>%s</li>",buf+1);
+}
+fclose(fileInput);
 
-  	fprintf(file,"%s","</ul></div>");
+fprintf(file,"%s","</ul></div>");
 
 
-  	fprintf(file,"<h1 style=\"text-align: center;\" > Generation of the Class Scheduling by <a href=\"mailto:valentin.montmirail@gmail.com\">Valentin Montmirail</a></h1>");
-  	fprintf(file,"<h2 style=\"text-align: center;\" > Creating file + Solving + Generation HTML time : %8.0f ms</h2>",generationTime);
+fprintf(file,"<h1 style=\"text-align: center;\" > Generation of the Class Scheduling by <a href=\"mailto:valentin.montmirail@gmail.com\">Valentin Montmirail</a></h1>");
+fprintf(file,"<h2 style=\"text-align: center;\" > Creating file + Solving + Generation HTML time : %8.0f ms</h2>",generationTime);
 
-	if(result == 0) fprintf(file,"<h3 style=\"text-align: center; color:red\"> This problem has been proved without any solution.</h3>");
-	
-	fprintf(file,"%s",footerHTML);
+if(result == 0) fprintf(file,"<h3 style=\"text-align: center; color:red\"> This problem has been proved without any solution.</h3>");
 
-	fclose(file);
+fprintf(file,"%s",footerHTML);
 
-	return filename;
+fclose(file);
+
+return filename;
 }
 
